71da185d30186a7d7d3bb681bf879f94ea5c01ba
muningg <noesamae@gmail.com>
2024-10-05 10:39:06 +0800

    feat(lecture-list-service): add service to load lecture list

--- src/app/components/lecture-list/lecture-list.component.ts
+++ src/app/components/lecture-list/lecture-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
+import { LectureListService } from './lecture-list.service';
 
-// Define the Lecture interface
 interface Lecture {
   course: string;
   code: string;
@@ -17,60 +17,11 @@ interface Lecture {
 @Component({
   selector: 'app-lecture-list',
   templateUrl: './lecture-list.component.html',
-  styleUrls: ['./lecture-list.component.css']  
+  styleUrls: ['./lecture-list.component.css']
 })
 export class LectureListComponent {
-  lectures: Lecture[] = [
-    {
-      course: 'INFORMATION ASSURANCE AND SECURITY 2',
-      code: 'IAS 102',
-      section: 'BSIT-4A',
-      year: '4th Year',
-      semester: '1st Semester',
-      schoolYear: '2024-2025',
-      professor: 'PROF. EDGARDO CRUZ',
-      day: 'SAT',
-      time: '02:00 PM - 05:00 PM',
-      duration: '3 Hrs'
-    },
-    {
-      course: 'SYSTEM INTEGRATION AND ARCHITECTURE 2',
-      code: 'SIA 102',
-      section: 'BSIT-4A',
-      year: '4th Year',
-      semester: '1st Semester',
-      schoolYear: '2024-2025',
-      professor: 'MR. JOSE GAYARES III',
-      day: 'SAT',
-      time: '06:00 PM - 09:00 PM',
-      duration: '3 Hrs'
-    },
-    {
-      course: 'SYSTEM ADMINISTRATION AND MAINTENANCE',
-      code: 'SA 101',
-      section: 'BSIT-4A',
-      year: '4th Year',
-      semester: '1st Semester',
-      schoolYear: '2024-2025',
-      professor: 'MR. WILBER SABADO',
-      day: 'MON',
-      time: '06:00 PM - 09:00 PM',
-      duration: '3 Hrs'
-    },
-    {
-      course: 'CAPSTONE PROJECT 1',
-      code: 'CAP 101',
-      section: 'BSIT-4A',
-      year: '4th Year',
-      semester: '1st Semester',
-      schoolYear: '2024-2025',
-      professor: 'MR. PERCIVAL ADAO',
-      day: 'SAT',
-      time: '11:00 AM - 02:00 PM',
-      duration: '3 Hrs'
-    }
-  ];
-  
+
+  lectures: Lecture[] = [];
   newLecture: Lecture = {
     course: '',
     code: '',
@@ -83,10 +34,16 @@ export class LectureListComponent {
     time: '',
     duration: ''
   };
-  
+
+  constructor(private lectureListService: LectureListService) {}
+
+  provideLecturesList() {
+    this.lectures = this.lectureListService.loadLectures();
+  }
+
   addLecture() {
     if (this.newLecture.course.trim()) {
-      this.lectures.push({ ...this.newLecture });
+      this.lectureListService.addLecture({ ...this.newLecture });
       this.newLecture = {
         course: '',
         code: '',
